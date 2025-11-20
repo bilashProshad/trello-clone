255492806726c3c24700aa50b796852cd3e72929
Bilash Prosad <pbilash64@gmail.com>
2025-11-21 01:52:41 +0600

    basic authentication added. users can sign up and sign in

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export default function ClerkLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="h-full flex items-center justify-center">{children}</div>
+  );
+}
