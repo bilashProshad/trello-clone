eda820329f9c212ca8b3f908c3a5bbfb5e505615
Bilash Prosad <pbilash64@gmail.com>
2025-11-23 00:19:12 +0600

    create hooks, schema, types, server action, and frontend/backend type safety

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod/v3";
+
+export const CreateBoard = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is required",
+    })
+    .min(3, { message: "Title is too short" }),
+});
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,27 @@
-export default function OrganizationIdPage() {
-  return <div>Organization</div>;
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+
+export default async function OrganizationIdPage() {
+  const boards = await prisma.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <form>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          placeholder="Enter a board title"
+          required
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>Board title: {board.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
