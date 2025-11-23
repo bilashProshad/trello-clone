0cd12f1f2a77f7e8c4a5b6e97e730b21be1af59e
Bilash Prosad <pbilash64@gmail.com>
2025-11-23 19:17:39 +0600

    created custom resuable form component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,22 +1,12 @@
-import { Button } from "@/components/ui/button";
 import prisma from "@/lib/db";
+import Form from "./form";
 
 export default async function OrganizationIdPage() {
   const boards = await prisma.board.findMany();
 
   return (
     <div className="flex flex-col space-y-4">
-      <form>
-        <input
-          id="title"
-          name="title"
-          type="text"
-          placeholder="Enter a board title"
-          required
-          className="border-black border p-1"
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+      <Form />
       <div className="space-y-2">
         {boards.map((board) => (
           <div key={board.id}>Board title: {board.title}</div>
