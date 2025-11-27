65de597282960faa5abeceaa8e3133a36b40365b
Bilash Prosad <pbilash64@gmail.com>
2025-11-27 20:41:30 +0600

    feat: integrate FormPopover for board creation and enhance BoardList with skeleton loading

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,20 @@
 import { ReactNode } from "react";
 import OrgControl from "./_components/org-control";
+import { startCase } from "lodash";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export async function generateMetadata() {
+  const { orgSlug } = await auth();
+
+  const client = await clerkClient();
+  const response = await client.organizations.getOrganization({
+    slug: orgSlug as string,
+  });
+
+  return {
+    title: startCase(response.name || "Organization"),
+  };
+}
 
 export default function OrganizationIdLayout({
   children,
