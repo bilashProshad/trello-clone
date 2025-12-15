73078921c6a97b41a9a4fcbefeed896df42bd36d
Bilash Prosad <pbilash64@gmail.com>
2025-12-15 22:42:40 +0600

    feat: implement create card functionality with form handling and validation

---
+++ actions/create-card/schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod/v3";
+
+export const CreateCard = z.object({
+  boardId: z.string(),
+  listId: z.string(),
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is required",
+    })
+    .min(3, { message: "Title is too short" }),
+});
