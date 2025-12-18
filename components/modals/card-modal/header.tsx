de567eb2293145d01f3be72de443c514d7b42dbc
Bilash Prosad <pbilash64@gmail.com>
2025-12-18 16:39:05 +0600

    feat: add audit log creation for card and list actions, and update query invalidation in card modal

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -25,6 +25,10 @@ export default function Header({ data }: HeaderProps) {
         queryKey: ["card", data.id],
       });
 
+      queryClient.invalidateQueries({
+        queryKey: ["card-logs", data.id],
+      });
+
       toast.success(`Renamed to "${data.title}`);
       setTitle(data.title);
     },
