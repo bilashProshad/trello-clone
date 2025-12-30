50cf4599b2b5258186e5f58e3aefb1514e709e18
Bilash Prosad <pbilash64@gmail.com>
2025-12-30 21:51:49 +0600

    feat: add subscription check to delete board action and adjust DAY_IN_MS constant

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -2,7 +2,7 @@
 import { auth } from "@clerk/nextjs/server";
 import prisma from "./db";
 
-const DAY_IN_MS = 84_400_000;
+const DAY_IN_MS = 86_400_000;
 
 export const checkSubscription = async () => {
   const { orgId } = await auth();
